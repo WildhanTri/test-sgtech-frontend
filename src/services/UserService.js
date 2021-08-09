9fd354a40c10cec1f6b82a10dc2f138227f05d48
WildhanTri <wildhan.triherlian@mogawe.id>
2021-08-09 10:35:56 +0700

    add userService

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,45 +1,19 @@
 import axios from "axios";
 import React from "react";
+import { BACKEND_ENDPOINT } from "../constants/constants";
 
 
 export default class UserService extends React.Component {
 
-    register = (name, email, password) => {
+    register = (user_email, user_first_name, user_last_name, user_password) => {
         var request = {
-            name: name,
-            email: email,
-            password: password
+            user_email: user_email,
+            user_first_name: user_first_name,
+            user_last_name: user_last_name,
+            user_password: user_password
         }
         return new Promise((resolve, reject) => {
-            axios.post("https://backendexample.sanbersy.com/api/register", request)
-                .then(
-                    (response) => {
-                        resolve(response.data)
-                    }
-                ).catch(
-                    (error) => {
-                        const errorMessage = JSON.parse(error.request.response)
-                        try {
-                            var errorMessageObj = JSON.parse(errorMessage)
-                            if (errorMessageObj.email)
-                                reject(errorMessageObj.email[0])
-                            if (errorMessageObj.password)
-                                reject(errorMessageObj.password[0])
-                        } catch (e) {
-                            reject(error)
-                        }
-                    }
-                )
-        })
-    }
-
-    login = (email, password) => {
-        var request = {
-            email: email,
-            password: password
-        }
-        return new Promise((resolve, reject) => {
-            axios.post("https://backendexample.sanbersy.com/api/user-login", request)
+            axios.post(BACKEND_ENDPOINT + "/api/register", request)
                 .then(
                     (response) => {
                         resolve(response.data)
@@ -58,15 +32,42 @@ export default class UserService extends React.Component {
         })
     }
 
-    changePassword = (password, new_password, new_confirm_password) => {
+    login = (user_email, user_password) => {
         var request = {
-            current_password: password,
-            new_password: new_password,
-            new_confirm_password: new_confirm_password
+            user_email: user_email,
+            user_password: user_password
+        }
+        return new Promise((resolve, reject) => {
+            axios.post(BACKEND_ENDPOINT + "/api/login", request)
+                .then(
+                    (response) => {
+                        resolve(response.data)
+                    }
+                ).catch(
+                    (error) => {
+                        const errorMessage = JSON.parse(error.request.response)
+                        try {
+                            if (errorMessage.error)
+                                reject(errorMessage.error)
+                        } catch (e) {
+                            reject(error)
+                        }
+                    }
+                )
+        })
+    }
+
+    updateProfile = (user) => {
+        var request = {
+            user_first_name: user.user_first_name,
+            user_last_name: user.user_last_name,
+            user_email: user.user_email,
+            user_birthday: user.user_birthday,
+            user_gender: user.user_gender
         }
 
         return new Promise((resolve, reject) => {
-            axios.post("https://backendexample.sanbersy.com/api/change-password", request, { headers: { "Authorization": "Bearer " + localStorage.getItem("token") } })
+            axios.patch(BACKEND_ENDPOINT + "/api/user/profile", request, { headers: { "Authorization": "Bearer " + localStorage.getItem("token") } })
                 .then(
                     (response) => {
                         resolve(response)
@@ -75,13 +76,34 @@ export default class UserService extends React.Component {
                     (error) => {
                         const errorMessage = JSON.parse(error.request.response)
                         try {
-                            var errorMessageObj = JSON.parse(errorMessage)
-                            if (errorMessageObj.current_password)
-                                reject(errorMessageObj.current_password[0])
-                            if (errorMessageObj.new_password)
-                                reject(errorMessageObj.new_password[0])
-                            if (errorMessageObj.new_confirm_password)
-                                reject(errorMessageObj.new_confirm_password[0])
+                            if (errorMessage.error)
+                                reject(errorMessage.error)
+                        } catch (e) {
+                            reject(error)
+                        }
+                    }
+                )
+        })
+    }
+
+    updatePassword = (old_password, new_password) => {
+        var request = {
+            old_password: old_password,
+            new_password: new_password
+        }
+
+        return new Promise((resolve, reject) => {
+            axios.patch(BACKEND_ENDPOINT + "/api/change-password", request, { headers: { "Authorization": "Bearer " + localStorage.getItem("token") } })
+                .then(
+                    (response) => {
+                        resolve(response)
+                    }
+                ).catch(
+                    (error) => {
+                        const errorMessage = JSON.parse(error.request.response)
+                        try {
+                            if (errorMessage.error)
+                                reject(errorMessage.error)
                         } catch (e) {
                             reject(error)
                         }
@@ -89,5 +111,32 @@ export default class UserService extends React.Component {
                 )
         })
 
+    }
+
+    forgotPassword = (user_email, user_password, user_password_confirm) => {
+        var request = {
+            user_email: user_email,
+            user_password: user_password,
+            user_password_confirm: user_password_confirm
+        }
+
+        return new Promise((resolve, reject) => {
+            axios.patch(BACKEND_ENDPOINT + "/api/forgotPassword", request, { headers: { "Authorization": "Bearer " + localStorage.getItem("token") } })
+                .then(
+                    (response) => {
+                        resolve(response)
+                    }
+                ).catch(
+                    (error) => {
+                        const errorMessage = JSON.parse(error.request.response)
+                        try {
+                            if (errorMessage.error)
+                                reject(errorMessage.error)
+                        } catch (e) {
+                            reject(error)
+                        }
+                    }
+                )
+        })
     }
 }
