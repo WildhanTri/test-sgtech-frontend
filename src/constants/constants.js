6aced65a56b4446d6277bdfcb67a1561bed2d2a0
WildhanTri <wildhan.triherlian@mogawe.id>
2021-08-10 14:43:18 +0700

    deploy

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
 // DEV
-export const BACKEND_ENDPOINT = "http://localhost:3000"
+// export const BACKEND_ENDPOINT = "http://localhost:3000"
 // PROD
-// export const BACKEND_ENDPOINT = "https://test-sgtech-backend.herokuapp.com"
+export const BACKEND_ENDPOINT = "https://test-sgtech-backend.herokuapp.com"
